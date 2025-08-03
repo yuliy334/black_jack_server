9e5870e2e593eed7420b4afcd2b341cd9fb4b37c
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-08-03 21:43:42 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 9000;
+
+
+
+
+const gameState: { gameCards: string[]; playerCards: string[]; dilerCards: string[] } = {
+    gameCards: [],
+    playerCards: [],
+    dilerCards: []
+};
+
+app.get('/games', (_req: Request, res: Response) => {
+    createGameCards();
+    addCard();
+    addCard();
+    res.send({
+        gameCards: gameState.gameCards,
+        playerCards: gameState.playerCards
+    });
+});
+app.get('/games/hit', (_req: Request, res: Response) => {
+
+    addCard();
+    res.send({
+        gameCards: gameState.gameCards,
+        playerCards: gameState.playerCards
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
+
+
+
+
+function createGameCards() {
+    const suits = ['♠', '♥', '♦', '♣'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const cards: string[] = [];
+
+    for (const suit of suits) {
+        for (const rank of ranks) {
+            cards.push(`${rank}${suit}`);
+        }
+    }
+
+    gameState.gameCards = cards;
+}
+function addCard() {
+    let newCard: string = gameState.gameCards[Math.floor(Math.random() * (gameState.gameCards.length))];
+    gameState.playerCards.push(newCard);
+    gameState.gameCards = gameState.gameCards.filter((card) => card != newCard);
+}
