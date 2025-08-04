e07739f0ad8a95285415f45205ce56d47552bb60
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-08-04 19:20:23 +0300

    player points check

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,30 @@ const PORT = 9000;
 
 
 
-const gameState: { gameCards: string[]; playerCards: string[]; dilerCards: string[] } = {
+const gameState: { gameCards: string[]; playerCards: string[]; playerPoints: number; dilerCards: string[] } = {
     gameCards: [],
     playerCards: [],
+    playerPoints: 0,
     dilerCards: []
 };
 
 app.get('/games', (_req: Request, res: Response) => {
     createGameCards();
     newGame();
-    checkCards();
     res.send({
-        gameCards: gameState.gameCards,
-        playerCards: gameState.playerCards,
-        dilerCards: gameState.dilerCards
+        dilerCards: gameState.dilerCards,
+        gameCards: gameState.playerPoints,
+        playerCards: gameState.playerCards
+
     });
 });
 app.get('/games/hit', (_req: Request, res: Response) => {
 
     addCard();
     res.send({
-        gameCards: gameState.gameCards,
-        playerCards: gameState.playerCards
+        dilerCards: gameState.dilerCards,
+        playerCards: gameState.playerCards,
+        playerPoints: gameState.playerPoints
     });
 });
 
@@ -53,9 +55,10 @@ function createGameCards() {
     gameState.gameCards = cards;
 }
 
-function newGame(){
+function newGame() {
     gameState.dilerCards = [];
     gameState.playerCards = [];
+    gameState.playerPoints = 0;
     addCard();
     addCard();
     dilerAddCard();
@@ -66,12 +69,32 @@ function addCard() {
     let newCard: string = gameState.gameCards[Math.floor(Math.random() * (gameState.gameCards.length))];
     gameState.playerCards.push(newCard);
     gameState.gameCards = gameState.gameCards.filter((card) => card != newCard);
+
+    let newCardValue: string = newCard.slice(0, -1);
+    let newCardNumValue: number = Number(newCardValue);
+
+    if (!isNaN(newCardNumValue)) {
+        gameState.playerPoints += newCardNumValue;
+        console.log(gameState.playerPoints);
+    }
+    else {
+        if (newCardValue == "A") {
+            if (gameState.playerPoints <= 10) {
+                gameState.playerPoints += 11;
+            }
+            else {
+                gameState.playerPoints += 1;
+            }
+
+        }
+        else {
+            gameState.playerPoints += 10;
+        }
+    }
+
 }
 function dilerAddCard() {
     let newCard: string = gameState.gameCards[Math.floor(Math.random() * (gameState.gameCards.length))];
     gameState.dilerCards.push(newCard);
     gameState.gameCards = gameState.gameCards.filter((card) => card != newCard);
-}
-function checkCards() {
-
 }
