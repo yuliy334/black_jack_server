3472515e22bd2121eae35902f887eb08f9dfc100
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-08-04 20:28:33 +0300

    win lose check

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,67 @@ const PORT = 9000;
 
 
 
-const gameState: { gameCards: string[]; playerCards: string[]; playerPoints: number; dilerCards: string[] } = {
+const gameState: { gameCards: string[]; playerCards: string[]; playerPoints: number; dilerCards: string[], dilerPoints: number; isWin: boolean; isLoose: boolean, isPush: boolean } = {
     gameCards: [],
     playerCards: [],
     playerPoints: 0,
-    dilerCards: []
+    dilerCards: [],
+    dilerPoints: 0,
+    isWin: false,
+    isLoose: false,
+    isPush: false
 };
 
-app.get('/games', (_req: Request, res: Response) => {
+app.get('/games', (req: Request, res: Response) => {
     createGameCards();
     newGame();
+    Check();
     res.send({
         dilerCards: gameState.dilerCards,
-        gameCards: gameState.playerPoints,
-        playerCards: gameState.playerCards
+        dilerPoints: gameState.dilerPoints,
+
+        playerCards: gameState.playerCards,
+        playerPoints: gameState.playerPoints,
+
+        isWin: gameState.isWin,
+        isLoose: gameState.isLoose,
+        isPush: gameState.isPush
 
     });
 });
-app.get('/games/hit', (_req: Request, res: Response) => {
+app.get('/games/hit', (req: Request, res: Response) => {
 
     addCard();
+    Check();
     res.send({
         dilerCards: gameState.dilerCards,
+        dilerPoints: gameState.dilerPoints,
+
         playerCards: gameState.playerCards,
-        playerPoints: gameState.playerPoints
+        playerPoints: gameState.playerPoints,
+
+        isWin: gameState.isWin,
+        isLoose: gameState.isLoose,
+        isPush: gameState.isPush
     });
 });
+app.get('/games/stand', (req: Request, res: Response) => {
+    while (gameState.dilerPoints < 17) {
+        dilerAddCard();
+    }
+    CheckAfterStand();
+    res.send({
+        dilerCards: gameState.dilerCards,
+        dilerPoints: gameState.dilerPoints,
 
+        playerCards: gameState.playerCards,
+        playerPoints: gameState.playerPoints,
+
+        isWin: gameState.isWin,
+        isLoose: gameState.isLoose,
+        isPush: gameState.isPush
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен: http://localhost:${PORT}`);
@@ -59,6 +93,10 @@ function newGame() {
     gameState.dilerCards = [];
     gameState.playerCards = [];
     gameState.playerPoints = 0;
+    gameState.dilerPoints = 0;
+    gameState.isWin = false;
+    gameState.isLoose = false;
+    gameState.isPush = false;
     addCard();
     addCard();
     dilerAddCard();
@@ -75,7 +113,6 @@ function addCard() {
 
     if (!isNaN(newCardNumValue)) {
         gameState.playerPoints += newCardNumValue;
-        console.log(gameState.playerPoints);
     }
     else {
         if (newCardValue == "A") {
@@ -97,4 +134,51 @@ function dilerAddCard() {
     let newCard: string = gameState.gameCards[Math.floor(Math.random() * (gameState.gameCards.length))];
     gameState.dilerCards.push(newCard);
     gameState.gameCards = gameState.gameCards.filter((card) => card != newCard);
+
+    let newCardValue: string = newCard.slice(0, -1);
+    let newCardNumValue: number = Number(newCardValue);
+
+    if (!isNaN(newCardNumValue)) {
+        gameState.dilerPoints += newCardNumValue;
+    }
+    else {
+        if (newCardValue == "A") {
+            if (gameState.dilerPoints <= 10) {
+                gameState.dilerPoints += 11;
+            }
+            else {
+                gameState.dilerPoints += 1;
+            }
+
+        }
+        else {
+            gameState.dilerPoints += 10;
+        }
+    }
+}
+function Check(){
+    if (gameState.playerPoints > 22) {
+        gameState.isLoose = true;
+    }
+}
+function CheckAfterStand() {
+    if (gameState.playerPoints < 22) {
+        if (gameState.dilerPoints > 21) {
+            gameState.isWin = true;
+        }
+        else {
+            if (gameState.playerPoints > gameState.dilerPoints) {
+                gameState.isWin = true;
+            }
+            else if (gameState.playerPoints < gameState.dilerPoints) {
+                gameState.isLoose = true;
+            }
+            else {
+                gameState.isPush = true;
+            }
+        }
+    }
+    else {
+        gameState.isLoose = true;
+    }
 }
